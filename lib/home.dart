4919d50c2c4ff7520897a8abf63dbcc761458000
Salman <salmanprof9@gmail.com>
2023-11-28 13:03:03 +0530

    Added The Search Bar And Horizontal Navigation bar for Category Seach

--- lib/home.dart
+++ lib/home.dart
@@ -8,14 +8,96 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController searchController = new TextEditingController();
+  List<String> navBarItem=['Top News',"India" , "World" , "Finance" , "Health"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Newz"),
+        backgroundColor: Colors.blue,
+        title: Text("NEWZ" , style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
         centerTitle: true,
       ),
 
+      body: Column(
+        children: [
+
+          //Search Container
+          Container(
+
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24)),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if ((searchController.text).replaceAll(" ", "") ==
+                        "") {
+                      print("Blank search");
+                    } else {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                    }
+                  },
+                  child: Container(
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.blueAccent,
+                    ),
+                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (value){
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search Health"),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+
+          Container(
+            height: 50,
+            child: ListView.builder(
+              shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: navBarItem.length,
+                itemBuilder: (context , index){
+                  return InkWell(
+                    onTap: (){
+                      print(navBarItem[index]);
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      child: Text(navBarItem[index],
+                        style: TextStyle(
+                          fontSize: 19,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),),
+                    ),
+                  );
+                },
+            ),
+          )
+
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
