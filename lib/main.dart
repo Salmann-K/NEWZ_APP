e9b3dffadede5acb7971454c97798d352bc22abd
Salman <salmanprof9@gmail.com>
2023-11-30 18:48:50 +0530

    Added The Google Sign In Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newz/login.dart';
 
 import 'home.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: LoginScreen(),
     );
   }
 }
